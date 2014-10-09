19be6b6b7e2dc041e4880a8a3d89b487a99ba815
Sam <sanny.singh@tagitmobile.com>
2014-10-09 12:22:56 +0800

    detail and search complete

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
 //app.js
 var app = angular.module('flickgular', ['ngRoute']);
 var photos = [];
+var api = {
+	url : "https://api.flickr.com/services/rest",
+	methods : {
+		search :  "flickr.photos.search",
+		info : "flickr.photos.getInfo"
+	},
+	key : "77a30ff3e5b6d3ac7075a11ad8c35cdc",
+	secret : "2ad7071bcfd6b406",
+	format : "json&nojsoncallback=1"
+};
+
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 	.when('/photos', {
@@ -14,29 +25,63 @@ app.config(['$routeProvider', function ($routeProvider) {
 	.otherwise({ redirectTo: '/photos' })
 
 }]);
-app.controller('PhotosCtrl', ['$scope','$http', function ($scope,$http) {
+app.controller('PhotosCtrl', ['$scope','$log','$http', function ($scope,$log,$http) {
 		
 		$scope.photos = photos || [];
 		
 		$scope.search = function(text){
-			$http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3b79da6149f6cd056aae1f2a193df93f&text='+text+'&format=json&nojsoncallback=1')
+			$http.get(api.url+'/?method='+api.methods.search+'&api_key='+api.key+'&text='+text+'&format='+api.format)
 			.success(function(data){
-				photos = data.photos.photo;
-				$scope.photos = photos;
+				$scope.error = false;
+				if (data.stat === "fail") {
+					$log.info(data);
+					$scope.error = true;
+					$scope.error_msg = data.message;
+				}else{
+					$scope.error = false;
+					photos = data.photos.photo;
+					$scope.photos = photos;
+
+				}
+				
+			})
+			.error(function(data){
+				$log.error(data);
 			});
 		}
+
+		$scope.resetError = function(){
+			$scope.error = false;
+		};
 }]);
 
-app.controller('PhotoDetailCtrl', ['$scope','$routeParams','$http', function ($scope,$routeParams,$http) {
-	  $scope.photo = {};
+app.controller('PhotoDetailCtrl', ['$scope','$log','$routeParams','$http', function ($scope,$log,$routeParams,$http) {
+	  
 	  $scope.id = $routeParams.photoId;
-	  $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e547239fc7b331f67b0ee0d5894792f3&photo_id='+$scope.id+'&format=json&nojsoncallback=1')
+	  $http.get(api.url+'/?method='+api.methods.info+'&api_key='+api.key+'&photo_id='+$scope.id+'&format='+api.format)
 	  .success(function(data){
-	  	var photo_info = data.photo;
-	  	$scope.photo.title = photo_info.title._content;
-	  	$scope.photo.date = photo_info.dates.posted;
-	  	$scope.photo.owner = photo_info.owner.username;
-	  	console.log(data);
+	  	$log.info(data);
+	  	var info = data.photo;
+	  	$scope.photo = {
+	  		"title" : info.title._content,
+	  		"date" : info.dateuploaded,
+	  		"owner" : info.owner.username,
+	  		"farm" : info.farm,
+	  		"secret" : info.secret,
+	  		"server" : info.server,
+	  		"comments" : [ ]
+	  	};
+	  })
+	  .error(function(data){
+			$log.error(data);
 	  });
 
-}])
+	  $scope.comment = {};
+
+	  $scope.addComment = function(comment){
+	  	$scope.photo.comments.push(comment);
+	  	$scope.comment = {};
+	  	$log.debug($scope.photo.comments);
+	  }
+
+}]);
